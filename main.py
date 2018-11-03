d2de63c9e5f14e830f42ddb37c438e1dc44aeb29
Zanark <kiodeba11@gmail.com>
2018-11-03 19:51:19 +0530

    basic functionanilty is working

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 import numpy as np
 
+Row = 6
+Col = 6
+
 def gen_board(row = 6 , col = 6):                         #generates board of 6 X 6 by default
     board = np.zeros((row,col))
+    global Row 
+    Row = row-1
+    global Col 
+    Col = col-1
     return board
 
-def drop_piece():
-    pass
+def ins_piece(board , row , col , piece):
+    board[row][col] = piece
 
-def is_loc_valid():
-    pass
+def is_loc_valid(board , col):
+    if(board[Row][col]) == 0:
+        return True
 
-def get_next_open_row():
-    pass
+def get_next_open_row(board , col):
+    for r in range(Row):
+        if board[r][col] == 0:
+            return r
 
 
 board = gen_board()                     #generated a board
+
+print(board)                            #printing the board
 
 GameOver = False                        #Game Over variable
 
@@ -23,11 +35,19 @@ turn = 1                                #Turn Counter
 while not GameOver:                     #Unless Game Over
 
     if( turn % 2 != 0):                 #Player One's Turn
-        move1 = int(input("Enter your move Player One"))
-        print(" Player One's move : " + str(move1))
+        move1 = int(input("Enter your move Player One\t"))
+        print("\n Player One's move : " + str(move1) + "\n\n")
+        if is_loc_valid(board , move1):
+            row = get_next_open_row(board , move1)
+            ins_piece(board , row , move1 , 1)
+        print(board)
 
     else:                               #Playe Two's Turn
-        move2 = int(input("Enter your move Player Two"))
-        print(" Player Two's move : " + str(move2))
+        move2 = int(input("Enter your move Player Two\t"))
+        print(" Player Two's move : " + str(move2) + "\n\n")
+        if is_loc_valid(board , move2):
+            row = get_next_open_row(board , move2)
+            ins_piece(board , row , move2 , 2)
+        print(board)
 
     turn += 1
