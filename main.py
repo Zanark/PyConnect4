feb1e3f055e31575b0d4f8b93b7ee34816004dc0
Zanark <kiodeba11@gmail.com>
2018-11-03 19:28:24 +0530

    Added turns for player 1 and two and some comments

---
+++ main.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+def gen_board():                         #generates board of 6 X 6
+    board = np.zeros((6,6))
+    return board
+
+def drop_piece():
+    pass
+
+def is_loc_valid():
+    pass
+
+def get_next_open_row():
+    pass
+    
+
+board = gen_board()                     #generated a board
+
+GameOver = False                        #Game Over variable
+
+turn = 1                                #Turn Counter
+
+while not GameOver:                     #Unless Game Over
+
+    if( turn % 2 != 0):                 #Player One's Turn
+        move1 = int(input("Enter your move Player One"))
+        print(" Player One's move : " + str(move1))
+
+    else:                               #Playe Two's Turn
+        move2 = int(input("Enter your move Player Two"))
+        print(" Player Two's move : " + str(move2))
+
+    turn += 1
