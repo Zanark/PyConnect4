a486d665f2d78c22dd931db2550018b9c9b5c9a7
Zanark <kiodeba11@gmail.com>
2018-11-03 20:11:14 +0530

    Flipped the board to display it like in real world

--- main.py
+++ main.py
@@ -15,13 +15,19 @@ def ins_piece(board , row , col , piece):
     board[row][col] = piece
 
 def is_loc_valid(board , col):
-    if(board[Row][col]) == 0:
-        return True
+    if (col <= Col):
+        if(board[Row][col]) == 0:
+            return True
+    else:
+        return False
 
 def get_next_open_row(board , col):
     for r in range(Row):
         if board[r][col] == 0:
             return r
+
+def print_flipped_board(board):
+    print(np.flip(board , 0))
 
 
 board = gen_board()                     #generated a board
@@ -35,19 +41,27 @@ turn = 1                                #Turn Counter
 while not GameOver:                     #Unless Game Over
 
     if( turn % 2 != 0):                 #Player One's Turn
-        move1 = int(input("Enter your move Player One\t")) - 1
-        print("\n Player One's move : " + str(move1) + "\n\n")
+        move1 = int(input("\n Turn No : " + str(turn) + "\tEnter your move Player One\t")) - 1
+        print("\n Turn No : " + str(turn) + "\t Player One's move : column " + str(move1+1) + "\n")
         if is_loc_valid(board , move1):
             row = get_next_open_row(board , move1)
             ins_piece(board , row , move1 , 1)
-        print(board)
+        else:
+            print("Invalid Move, Please Enter Again")
+            # turn += 1
+            continue
+        print_flipped_board(board)
 
     else:                               #Playe Two's Turn
-        move2 = int(input("Enter your move Player Two\t")) - 1
-        print(" Player Two's move : " + str(move2) + "\n\n")
+        move2 = int(input("\n Turn No : " + str(turn) + "\tEnter your move Player Two\t")) - 1
+        print("\n Turn No : " + str(turn) + "\t Player Two's move : column " + str(move2+1) + "\n")
         if is_loc_valid(board , move2):
             row = get_next_open_row(board , move2)
             ins_piece(board , row , move2 , 2)
-        print(board)
+        else:
+            print("Invalid Move, Please Enter Again")
+            # turn += 1
+            continue
+        print_flipped_board(board)
 
     turn += 1
